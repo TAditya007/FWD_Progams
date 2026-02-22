de51752f26a2e3fa240d1ca4e8781c53aeafdde0
TAditya007 <adityalap007@gmail.com>
2026-02-22 12:00:05 +0530

    class on exceptions and promisesand es6 features

---
+++ topic wise programs/app.js
@@ -0,0 +1,9 @@
+import multiply, {add, PI} from "./math.js";
+//document.writeln("The value of PI is: " + PI + "<br>");
+//document.writeln("The sum of 5 and 10 is: " + add(5, 10) + "<br>");
+//document.writeln("The product of 5 and 10 is: " + multiply(5, 10) + "<br>");
+
+document.body.innerHTML+=
+"Valuse of PI is: " + PI + "<br>" +
+"The sum of 5 and 10 is: " + add(5, 10) + "<br>" +
+"The product of 5 and 10 is: " + multiply(5, 10) + "<br>";
